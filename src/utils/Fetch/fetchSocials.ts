6757df24d4d414ae41028d5f582e7dd136c9fc05
grafek <jacek.grafender@gmail.com>
2023-03-14 18:12:13 +0100

    fix: api route fix

--- src/utils/Fetch/fetchSocials.ts
+++ src/utils/Fetch/fetchSocials.ts
@@ -1,6 +1,6 @@
 import { SocialMedia } from '../../../types';
 
-export default async function fetchLinkedIn() {
+export default async function fetchSocials() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
 
   const data = await res.json();
