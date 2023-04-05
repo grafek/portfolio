fd5538735539c9029eade71239a95342d4e8623a
grafek <jacek.grafender@gmail.com>
2023-04-05 18:40:33 +0200

    chore: remove unnecessary stuff rom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,28 +7,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      keyframes: {
-        slideIn: {
-          '0%': {
-            transform: 'translateX(-100%)',
-          },
-          '100%': {
-            transform: 'translateX(0%)',
-          },
-        },
-        slideOut: {
-          '0%': {
-            transform: 'translateX(0%)',
-          },
-          '100%': {
-            transform: 'translateX(-100%)',
-          },
-        },
-      },
-      animation: {
-        slideOut: 'slideOut 0.8s forwards',
-        slideIn: 'slideIn 0.8s forwards',
-      },
       colors: {
         darkBg: '#0F1624',
         gray: {
